361a65e4c4307c40fd9ce695580bb946ca71f3cd
Selma <shrustic1@etf.unsa.ba>
2023-07-30 17:31:00 +0200

    added private static method createConnection in AbstractDao class

---
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -0,0 +1,43 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Idable;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.*;
+public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+
+    private static Connection conn = null;
+    private String tableName;
+
+    public AbstractDao(String tableName) {
+        this.tableName = tableName;
+        createConnection();
+    }
+
+    private static void createConnection() {
+        if (conn == null){
+            try {
+                Properties p = new Properties();
+                p.load(ClassLoader.getSystemResource("application.properties").openStream());
+                String url = p.getProperty("db.connection_string");
+                String username = p.getProperty("db.username");
+                String password = p.getProperty("db.password");
+                conn = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally{
+                Runtime.getRuntime().addShutdownHook(new Thread(){
+                    @Override
+                    public void run(){
+                        try {
+                            conn.close();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
