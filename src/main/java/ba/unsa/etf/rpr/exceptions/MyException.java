6fefddf02a5c4d6df7b1a216138f9826c3a96550
Selma <shrustic1@etf.unsa.ba>
2023-07-30 16:40:48 +0200

    exception made

---
+++ src/main/java/ba/unsa/etf/rpr/exceptions/MyException.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.exceptions;
+
+public class MyException extends Exception{
+
+    public MyException(String msg, Exception reason){
+        super(msg, reason);
+    }
+
+    public MyException(String msg){
+        super(msg);
+    }
+}
