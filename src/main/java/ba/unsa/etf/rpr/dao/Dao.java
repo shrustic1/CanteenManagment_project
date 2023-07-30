7f65047be84b218563296537e8b1281c8044e2a1
Selma <shrustic1@etf.unsa.ba>
2023-07-30 16:40:16 +0200

    added methods in Dao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.exceptions.MyException;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    T getById(int id) throws MyException;
+
+    T add(T item) throws MyException;
+
+    T update(T item) throws MyException;
+
+    void delete(int id) throws MyException;
+
+    List<T> getAll() throws MyException;
+}
