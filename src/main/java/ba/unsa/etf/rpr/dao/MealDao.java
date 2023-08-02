4c3c48294e9884e697aa79518cf3b748954790b1
Selma <shrustic1@etf.unsa.ba>
2023-08-02 11:47:28 +0200

    declared method searchByStudent in MealDao interface

--- src/main/java/ba/unsa/etf/rpr/dao/MealDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MealDao.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Meal;
+import ba.unsa.etf.rpr.domain.Student;
 import ba.unsa.etf.rpr.exceptions.MyException;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 public interface MealDao extends Dao<Meal>{
      Meal randomMeal() throws MyException;
      List<Meal> searchByTaken(Boolean taken) throws MyException;
+
+     List<Meal> searchByStudent(Student student) throws MyException;
 }
