d18668c54a5674763fcabbb8aba62eb06e2f6344
Selma <shrustic1@etf.unsa.ba>
2023-08-02 12:53:28 +0200

    added overridden method row2object in StudentDaoSQLImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/StudentDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/StudentDaoSQLImpl.java
@@ -4,6 +4,7 @@ import ba.unsa.etf.rpr.domain.Student;
 import ba.unsa.etf.rpr.exceptions.MyException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -17,5 +18,39 @@ public class StudentDaoSQLImpl extends AbstractDao<Student> implements StudentDa
         return instance;
     }
 
+    @Override
+    public Student row2object(ResultSet rs) throws MyException {
+        try {
+            Student student = new Student();
+            student.setId(rs.getInt("id"));
+            student.setName(rs.getString("name"));
+            student.setSurname(rs.getString("surname"));
+            student.setNumberOfTokens(rs.getInt("number_of_tokens"));
+            return student;
+        } catch (SQLException e) {
+            throw new MyException(e.getMessage(), e);
+        }
+
+    }
+
+    @Override
+    public Map<String, Object> object2row(Student object) {
+        return null;
+    }
+
+    @Override
+    public List<Student> searchByName(String name) throws MyException {
+        return null;
+    }
+
+    @Override
+    public List<Student> searchBySurname(String surname) throws MyException {
+        return null;
+    }
+
+    @Override
+    public int searchByNumberOfTokens(int number) throws MyException {
+        return 0;
+    }
 
 }
