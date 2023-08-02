a0f463e8c316fd3d50dab03cde43bc26abdf150a
Selma <shrustic1@etf.unsa.ba>
2023-08-02 12:48:18 +0200

    added private attribute instance in StudentDaoSQLImpl class

---
+++ src/main/java/ba/unsa/etf/rpr/dao/StudentDaoSQLImpl.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Student;
+import ba.unsa.etf.rpr.exceptions.MyException;
+
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+
+public class StudentDaoSQLImpl extends AbstractDao<Student> implements StudentDao{
+    private static StudentDaoSQLImpl instance = null;
+
+}
