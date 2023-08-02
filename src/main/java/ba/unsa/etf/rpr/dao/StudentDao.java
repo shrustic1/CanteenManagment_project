25d8bfee3aaf5bd408699318b1d8b79ccc0d94e2
Selma <shrustic1@etf.unsa.ba>
2023-08-02 12:05:53 +0200

    declared method searchByName in StudentDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/StudentDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Student;
+import ba.unsa.etf.rpr.exceptions.MyException;
+
+import java.util.List;
+
+public interface StudentDao extends Dao<Student> {
+    List<Student> searchByName(String name) throws MyException;
+
+}
