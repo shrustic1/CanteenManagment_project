a2bca9363e21cd7e8729805645ce90e3d0ddae9a
Selma <shrustic1@etf.unsa.ba>
2023-08-02 11:35:07 +0200

    added private attribute instance in MealDaoSQLImpl class

---
+++ src/main/java/ba/unsa/etf/rpr/dao/MealDaoSQLImpl.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Meal;
+import ba.unsa.etf.rpr.exceptions.MyException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MealDaoSQLImpl extends AbstractDao<Meal> implements MealDao{
+
+    private static MealDaoSQLImpl instance = null;
+
+}
