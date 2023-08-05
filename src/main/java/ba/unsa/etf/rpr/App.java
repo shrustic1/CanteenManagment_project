ce65db44715d7c3d1393b4be00657fac1c473e53
Selma <shrustic1@etf.unsa.ba>
2023-08-05 16:58:01 +0200

    added public static method searchThroughCooks in App class

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -3,10 +3,11 @@ package ba.unsa.etf.rpr;
 import ba.unsa.etf.rpr.business.CookManager;
 import ba.unsa.etf.rpr.business.MealManager;
 import ba.unsa.etf.rpr.business.StudentManager;
+import ba.unsa.etf.rpr.domain.Cook;
 import ba.unsa.etf.rpr.domain.Meal;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
+import ba.unsa.etf.rpr.domain.Student;
+import ba.unsa.etf.rpr.exceptions.MyException;
+import org.apache.commons.cli.*;
 
 import java.io.PrintWriter;
 import java.util.List;
@@ -47,5 +48,31 @@ public class App {
         Meal meal = null;
         meal = listOfMeals.stream().filter(m -> m.getName().toLowerCase().equals(nameOfMeal.toLowerCase())).findAny().get();
         return meal;
+    }
+
+    public static Cook searchThroughCooks(List<Cook> listOfCooks, String nameOfCook){
+        Cook cook = null;
+        cook = listOfCooks.stream().filter(c -> c.getName().toLowerCase().equals(nameOfCook.toLowerCase())).findAny().get();
+        return cook;
+    }
+
+    public static void main(String[] args) throws MyException, ParseException {
+        Options options = addOptions();
+        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine cl = commandLineParser.parse(options, args);
+
+
+        if (cl.hasOption(addMeal.getOpt()) || cl.hasOption(addMeal.getLongOpt())){
+            Student student = null;
+            Cook cook = null;
+            cook = searchThroughCooks(cookManager.getAll(), cl.getArgList().get(1));
+
+            Meal meal = new Meal();
+            meal.setTaken(Boolean.valueOf(cl.getArgList().get(2)));
+            meal.setDescription(cl.getArgList().get(1));
+            meal.setName(cl.getArgList().get(5));
+            meal.setStudent(null);
+            meal.setCook(cook);
+        }
     }
 }
